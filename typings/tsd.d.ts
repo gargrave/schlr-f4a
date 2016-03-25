c4a1d99319aec2362e04763742dff36b7a29a847
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-03-25 15:12:31 -0700

    initial commit

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="angularjs/angular-cookies.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="fastclick/fastclick.d.ts" />
